045ca6c086796f1bf1645aa02353198156667aee
tobi <tobi.klatt@web.de>
2022-03-21 20:37:21 +0100

    add searchbar and results to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
+    <div class="mt-10">
         <searchbar />
+        <div v-if="userChoice">
+            <result class="mx-auto mt-3 w-96" :result="userChoice" />
+            <result class="mx-auto mt-3 w-96" v-for="(result) in otherResultsOfType" :key="result.id" :result="result" />
+        </div>
     </div>
 </template>
 
+
 <script>
 import searchbar from '@/components/ui/searchbar'
+import result from '@/components/ui/result'
 
 export default {
     components: {
-        searchbar
+        searchbar,
+        result
+    },
+
+    computed: {
+        artists () { return this.$store.getters['artists'] },
+        albums () { return this.$store.getters['albums'] },
+        songs () { return this.$store.getters['songs'] },
+        userChoice () { return this.$store.getters['userChoice'] },
+        otherResultsOfType () { return this.$store.getters['otherResultsOfType'] },
     }
 }
 </script>
