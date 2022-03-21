ce8c8cc3e98b98b96ed15ecaf8113fdb1bae79a7
tobi <tobi.klatt@web.de>
2022-03-21 20:35:46 +0100

    add components to use svg icons

---
+++ components/icons/abstractSvg.vue
@@ -0,0 +1,23 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" :class="css" :fill="fill" viewBox="0 0 24 24" :stroke="stroke">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" :d="path" />
+    </svg>
+</template>
+
+<script>
+export default {
+    props: {
+        path: {
+            required: true
+        },
+        css: {},
+        fill: {
+            default: 'none'
+        },
+        stroke: {
+            default: 'currentColor'
+        },
+
+    }
+}
+</script>
---
+++ components/icons/iconSearch.vue
@@ -0,0 +1,18 @@
+<template>
+    <abstractSvg :path="path" :class="css" stroke="#6B7280" />
+</template>
+
+<script>
+import abstractSvg from '@/components/icons/abstractSvg'
+export default {
+    components: {
+        abstractSvg
+    },
+    props: ['css'],
+    data () {
+        return {
+            path: "M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        } 
+    }
+}
+</script>
