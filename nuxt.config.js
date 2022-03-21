5adac63e0380445a16b691f6be9f1866dcfb3c66
tobi <tobi.klatt@web.de>
2022-03-21 20:33:50 +0100

    add proxy for iTunes API

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,10 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
+  
+  proxy: {
+    '/itunes_proxy/': { target: 'https://itunes.apple.com/', pathRewrite: {'^/itunes_proxy/': ''} },
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
